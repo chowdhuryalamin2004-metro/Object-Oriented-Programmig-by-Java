fd88497565a2d97bb6a08a382c52fe94b395aecf
chowdhuryalamin2004-metro <chowdhuryalamin2004@gmail.com>
2026-02-16 17:08:15 +0600

    Create takinginputfromuser.java

---
+++ takinginputfromuser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner alamin = new Scanner(System.in);
+
+        System.out.println("Enter your valuable value:");
+        String nam = alamin.nextLine();
+
+        System.out.println(nam);
+    }
+}
